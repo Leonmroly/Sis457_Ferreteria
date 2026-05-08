1a36bb5ca68e2fe0f405cce3ee5bea7d91b35d9d
Leonmroly <tu_correo@gmail.com>
2026-05-08 15:54:32 -0400

    detalles en FrmMarca

--- Ferreteria/CpFerreteria/FrmMarcaEntry.Designer.cs
+++ Ferreteria/CpFerreteria/FrmMarcaEntry.Designer.cs
@@ -38,7 +38,7 @@
             // 
             this.btnGuardar.Image = global::CpFerreteria.Properties.Resources.save;
             this.btnGuardar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnGuardar.Location = new System.Drawing.Point(121, 129);
+            this.btnGuardar.Location = new System.Drawing.Point(73, 102);
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Size = new System.Drawing.Size(108, 40);
             this.btnGuardar.TabIndex = 19;
@@ -51,7 +51,7 @@
             // 
             this.btnCancelar.Image = global::CpFerreteria.Properties.Resources.cancel;
             this.btnCancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCancelar.Location = new System.Drawing.Point(292, 129);
+            this.btnCancelar.Location = new System.Drawing.Point(208, 102);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(108, 40);
             this.btnCancelar.TabIndex = 20;
@@ -63,7 +63,7 @@
             // lblNombreMarca
             // 
             this.lblNombreMarca.AutoSize = true;
-            this.lblNombreMarca.Location = new System.Drawing.Point(74, 82);
+            this.lblNombreMarca.Location = new System.Drawing.Point(69, 9);
             this.lblNombreMarca.Name = "lblNombreMarca";
             this.lblNombreMarca.Size = new System.Drawing.Size(155, 20);
             this.lblNombreMarca.TabIndex = 21;
@@ -71,7 +71,7 @@
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(258, 79);
+            this.txtNombre.Location = new System.Drawing.Point(73, 44);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(243, 26);
             this.txtNombre.TabIndex = 22;
@@ -80,7 +80,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(529, 217);
+            this.ClientSize = new System.Drawing.Size(387, 179);
             this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.lblNombreMarca);
             this.Controls.Add(this.btnCancelar);
@@ -88,6 +88,7 @@
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmMarcaEntry";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmMarcaEntry";
             this.ResumeLayout(false);
             this.PerformLayout();
